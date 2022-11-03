319e0be1134a53d1ce85519c2d71e2a0a41b4f5f
Alex Weiss <ajw282@cornell.edu>
2022-11-03 15:52:18 -0400

    Made some minor stylistic changes and commented code.

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
+// import dependencies
 const { Socket } = require("dgram")
 const express = require("express")
 const http = require('http')
+const path = require('path')
+const { readFileSync, writeFileSync } = require('fs')
+const { Server } = require('socket.io')
+const cheerio = require('cheerio')
+
+// create server
 const app = express()
 const server = http.createServer(app)
-const path = require('path')
-const { Server } = require('socket.io')
 const io = new Server(server)
-const { readFileSync, writeFileSync } = require('fs')
-const cheerio = require('cheerio')
+
+// load template index.html into public folder
 const $ = cheerio.load(readFileSync(path.join(__dirname, './template/index.html')))
 const template = $
-writeFileSync('./public/index.html', template.root().html())
+writeFileSync(path.join(__dirname, './public/index.html'), template.root().html())
+
+// start with no users or games
 let num_users = 0
 let num_games = 0
 
 app.get('/game/:game_id', (req, res) => {
+  // TODO: Implement game logic
   res.write("game")
   res.end()
 })
 
-
 app.use(express.static(path.join(__dirname, './public')))
 
 io.on('connection', (socket) => {
-  num_users += 1
-  io.emit('num users updated', num_users)
 
+  num_users += 1
   socket.on('disconnect', () => {
     num_users -= 1
-    io.emit('num users updated', num_users)
   })
 
   socket.on('entering game', () => {
@@ -38,8 +43,10 @@ io.on('connection', (socket) => {
 
   socket.on('add game', (element) => {
     io.emit('add game', element)
+    // Add link to newly-created game to dropdown menu and push new HTML to public
     $("#dropdown-container").append(element)
-    writeFileSync('./public/index.html', $.root().html())
+    $("#existinggame").prop("class", "active-button")
+    writeFileSync(path.join(__dirname, './public/index.html'), $.root().html())
   })
 })
 
